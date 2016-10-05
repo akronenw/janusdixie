bf0bb856e97b90f8bb7fce164f6d67fb5c5dceef
Patrick Zimmer <axel.evil@gmail.com>
2016-10-05 21:17:27 +0200

    added test for the persistence logic

--- src/test/java/org/afk/jadi/api/JanusDixieTest.java
+++ src/test/java/org/afk/jadi/api/JanusDixieTest.java
@@ -1,6 +1,12 @@
 package org.afk.jadi.api;
 
+import org.afk.jadi.impl.defaults.MappedJaDiMemory;
+import org.afk.jadi.impl.defaults.StaticJaDiManipulator;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import org.junit.Test;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doReturn;
 
 import static org.mockito.Mockito.mock;
 
@@ -11,27 +17,75 @@ public class JanusDixieTest {
 
     public static final String BAMAN_PIDERMAN = "Baman & Piderman";
 
-
     private JaDiRecordSet<String> rs;
 
+    private JaDiMemory memory;
+    private JaDiPersistence persistence;
+    private JaDiManipulator manipulator;
+    private JanusDixie janusDixie;
+    private String value;
 
     @Test
+    @SuppressWarnings("ResultOfObjectAllocationIgnored")
     public void nonNullParametersAreAccepted() {
         new JanusDixie(mock(JaDiMemory.class), mock(JaDiPersistence.class), mock(JaDiManipulator.class));
     }
 
     @Test(expected = NullPointerException.class)
+    @SuppressWarnings("ResultOfObjectAllocationIgnored")
     public void nullMemoryParameterIsNotAccepted() {
         new JanusDixie(null, mock(JaDiPersistence.class), mock(JaDiManipulator.class));
     }
 
     @Test(expected = NullPointerException.class)
+    @SuppressWarnings("ResultOfObjectAllocationIgnored")
     public void nullPersistenceParameterIsNotAccepted() {
         new JanusDixie(mock(JaDiMemory.class), null, mock(JaDiManipulator.class));
     }
 
     @Test(expected = NullPointerException.class)
+    @SuppressWarnings("ResultOfObjectAllocationIgnored")
     public void nullManipulatorParameterIsNotAccepted() {
         new JanusDixie(mock(JaDiMemory.class), mock(JaDiPersistence.class), null);
+    }
+
+    @Test
+    public void persistenceWins() throws JaDiException {
+        givenDixie();
+        whithPersistedValue("key", "beer");
+        whenAddedConfig("key", "coffee");
+        thenCallBackIs("beer");
+    }
+
+    @Test
+    public void defaultWins() throws JaDiException {
+        givenDixie();
+        whithPersistedValue("i", "beer");
+        whenAddedConfig("key", "coffee");
+        thenCallBackIs("coffee");
+    }
+
+    private void thenCallBackIs(String expected) {
+        assertThat(value, is(expected));
+    }
+
+    private JaDiRecordSet<String> whenAddedConfig(String key, String defaultValue) throws JaDiException {
+        return janusDixie.addOrGet(key, this::callBack, () -> defaultValue, String.class);
+    }
+
+    private void givenDixie() {
+        memory = new MappedJaDiMemory();
+        persistence = mock(JaDiPersistence.class);
+        manipulator = new StaticJaDiManipulator();
+        janusDixie = new JanusDixie(memory, persistence, manipulator);
+    }
+
+    private void whithPersistedValue(String key, String value) {
+        doReturn(true).when(persistence).has(eq(key));
+        doReturn(value).when(persistence).retrieve(eq(key), eq(String.class));
+    }
+
+    private void callBack(String value) {
+        this.value = value;
     }
 }
