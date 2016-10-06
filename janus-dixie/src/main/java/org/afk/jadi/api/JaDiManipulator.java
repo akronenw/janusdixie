3bc39f9dbae29c945b8c053f38ac5e548c0568f2
Patrick Zimmer <axel.evil@gmail.com>
2016-10-06 23:18:33 +0200

    added method to manipulator interface
    first solution just delegates the recordsets to the manipulator.

--- janus-dixie/src/main/java/org/afk/jadi/api/JaDiManipulator.java
+++ janus-dixie/src/main/java/org/afk/jadi/api/JaDiManipulator.java
@@ -1,10 +1,19 @@
 package org.afk.jadi.api;
 
 /**
- * This interface describes a manipulator of configuration.
- * The manipulator is a user interface that allows modification of configuration values.
- * Created by axel on 28.10.15.
+ * This interface describes a manipulator of configuration. The manipulator is a
+ * user interface that allows modification of configuration values. Created by
+ * axel on 28.10.15.
  */
 public interface JaDiManipulator {
+
+    /**
+     * This method is called whenever a new configuration was added.
+     *
+     * @param <T> The type of the configuration value.
+     * @param recordSet The JaDiRecordSet that provides all information of the
+     * config.
+     */
+    <T> void register(JaDiRecordSet<T> recordSet);
 
 }
--- janus-dixie/src/main/java/org/afk/jadi/defaults/StaticJaDiManipulator.java
+++ janus-dixie/src/main/java/org/afk/jadi/defaults/StaticJaDiManipulator.java
@@ -1,12 +1,16 @@
 package org.afk.jadi.defaults;
 
 import org.afk.jadi.api.JaDiManipulator;
+import org.afk.jadi.api.JaDiRecordSet;
 
 /**
- * The Static JaDiManipulator never changes any configuration values.
- * Created by axel on 28.10.15.
+ * The Static JaDiManipulator never changes any configuration values. Created by
+ * axel on 28.10.15.
  */
 public class StaticJaDiManipulator implements JaDiManipulator {
 
+    @Override
+    public <T> void register(JaDiRecordSet<T> recordSet) {
+    }
 
 }
