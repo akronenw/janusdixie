b5b5aace52d696876773897c3edb9a40a4c80d89
Patrick Zimmer <axel.evil@gmail.com>
2016-10-06 21:35:48 +0200

    fixed SimpleStringConverterFactory
    now the correct Factory is returned or an exception thrown.

--- janus-dixie-persistence-preferences/src/test/java/org/afk/jadi/impl/persist/prefs/JaDiPreferencePersistenceTest.java
+++ janus-dixie-persistence-preferences/src/test/java/org/afk/jadi/impl/persist/prefs/JaDiPreferencePersistenceTest.java
@@ -1,6 +1,5 @@
 package org.afk.jadi.impl.persist.prefs;
 
-import org.afk.jadi.api.JaDiException;
 import org.afk.jadi.tools.SimpleStringConverterFactory;
 import org.junit.After;
 import org.junit.Test;
@@ -10,53 +9,92 @@ import java.util.prefs.Preferences;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 /**
  * Created by axel on 23.11.15.
  */
 public class JaDiPreferencePersistenceTest {
 
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    
     private Preferences prefs;
     private JaDiPreferencePersistence jaDiPreferencePersistence;
 
+    /**
+     * Clean the preferences after testing.
+     *
+     * @throws BackingStoreException
+     */
     @After
     public void removeNode() throws BackingStoreException {
         Preferences.userNodeForPackage(this.getClass()).removeNode();
     }
 
+    /**
+     * test that there is no stored value in fresh preference persistence.
+     */
     @Test
     public void testPrefsHasNotUnsetValue() {
         givenPrefs();
         withPersistence();
+
         assertThat(jaDiPreferencePersistence.has("a"), not(equalTo(true)));
     }
 
+    /**
+     * test that a stored value is available.
+     */
     @Test
-    public void testPrefsHasStoredValue() throws JaDiException {
+    public void testPrefsHasStoredValue() {
         givenPrefs();
         withPersistence();
         jaDiPreferencePersistence.store("a", "b", String.class);
-        assertThat(jaDiPreferencePersistence.has("a"), equalTo(true));
+
+        assertThat(jaDiPreferencePersistence.has("a"), is(true));
     }
 
+    /**
+     * test that a stored string is retrieved.
+     */
     @Test
-    public void testPrefsProvideStoredValue() throws JaDiException {
+    public void testPrefsProvideStoredValue() {
         givenPrefs();
         withPersistence();
         jaDiPreferencePersistence.store("a", "b", String.class);
         String result = jaDiPreferencePersistence.retrieve("a", String.class);
-        assertThat(result, equalTo("b"));
+        assertThat(result, is("b"));
     }
 
+    /**
+     * test that a stored double is retrieved.
+     */
     @Test
-    public void testPrefsProvideStoredDoubleValue() throws JaDiException {
+    public void testPrefsProvideStoredDoubleValue() {
         givenPrefs();
         withPersistence();
         final double t = 24.35;
         jaDiPreferencePersistence.store("a", t, Double.class);
         Double result = jaDiPreferencePersistence.retrieve("a", Double.class);
-        assertThat(result, equalTo(t));
+        assertThat(result, is(t));
+    }
+
+    /**
+     * test that a converter for Object can not be retrieved.
+     */
+    @Test
+    public void testPrefsProvideStoredObjectValue() {
+        givenPrefs();
+        withPersistence();
+        final double t = 24.35;
+        
+        exception.expect(RuntimeException.class);
+        exception.expectMessage("org.afk.jadi.api.JaDiException: No Converter for class class java.lang.Object defined");
+        jaDiPreferencePersistence.store("a", t, Object.class);
     }
 
     private void withPersistence() {
--- janus-dixie/src/main/java/org/afk/jadi/tools/SimpleStringConverterFactory.java
+++ janus-dixie/src/main/java/org/afk/jadi/tools/SimpleStringConverterFactory.java
@@ -9,8 +9,9 @@ import java.math.BigInteger;
 import java.util.Objects;
 
 /**
- * The SimpleStringConverterFactory provides JaDiStringConverter for String, Byte, Short, Integer, Long, BigInteger, Float, Double and BigDecimal.
- * If you require more conversions it is safe to extend this factory, overwrite the
+ * The SimpleStringConverterFactory provides JaDiStringConverter for String,
+ * Byte, Short, Integer, Long, BigInteger, Float, Double and BigDecimal. If you
+ * require more conversions it is safe to extend this factory, overwrite the
  * Created by axel on 08.11.2015.
  */
 public class SimpleStringConverterFactory implements JaDiStringConverterFactory {
@@ -20,26 +21,35 @@ public class SimpleStringConverterFactory implements JaDiStringConverterFactory 
 
         Objects.requireNonNull(clazz, "The Class parameter must not be null");
 
-        if (clazz.isAssignableFrom(String.class))
-            return new JaDiStringConverter<T>(s -> clazz.cast(s), i -> String.valueOf(i));
+        if (String.class.isAssignableFrom(clazz)) {
+            return new JaDiStringConverter<>(s -> clazz.cast(s), i -> String.valueOf(i));
+        }
 
-        if (clazz.isAssignableFrom(Byte.class))
-            return new JaDiStringConverter<T>(s -> clazz.cast(Byte.valueOf(s)), i -> String.valueOf(i));
-        if (clazz.isAssignableFrom(Short.class))
-            return new JaDiStringConverter<T>(s -> clazz.cast(Short.valueOf(s)), i -> String.valueOf(i));
-        if (clazz.isAssignableFrom(Integer.class))
-            return new JaDiStringConverter<T>(s -> clazz.cast(Integer.valueOf(s)), s -> s.toString());
-        if (clazz.isAssignableFrom(Long.class))
-            return new JaDiStringConverter<T>(s -> clazz.cast(Long.valueOf(s)), i -> String.valueOf(i));
-        if (clazz.isAssignableFrom(BigInteger.class))
-            return new JaDiStringConverter<T>(s -> clazz.cast(new BigInteger(s)), i -> String.valueOf(i));
+        if (Byte.class.isAssignableFrom(clazz)) {
+            return new JaDiStringConverter<>(s -> clazz.cast(Byte.valueOf(s)), i -> String.valueOf(i));
+        }
+        if (Short.class.isAssignableFrom(clazz)) {
+            return new JaDiStringConverter<>(s -> clazz.cast(Short.valueOf(s)), i -> String.valueOf(i));
+        }
+        if (Integer.class.isAssignableFrom(clazz)) {
+            return new JaDiStringConverter<>(s -> clazz.cast(Integer.valueOf(s)), s -> s.toString());
+        }
+        if (Long.class.isAssignableFrom(clazz)) {
+            return new JaDiStringConverter<>(s -> clazz.cast(Long.valueOf(s)), i -> String.valueOf(i));
+        }
+        if (BigInteger.class.isAssignableFrom(clazz)) {
+            return new JaDiStringConverter<>(s -> clazz.cast(new BigInteger(s)), i -> String.valueOf(i));
+        }
 
-        if (clazz.isAssignableFrom(Float.class))
-            return new JaDiStringConverter<T>(s -> clazz.cast(Float.valueOf(s)), i -> String.valueOf(i));
-        if (clazz.isAssignableFrom(Double.class))
-            return new JaDiStringConverter<T>(s -> clazz.cast(Double.valueOf(s)), i -> String.valueOf(i));
-        if (clazz.isAssignableFrom(BigDecimal.class))
-            return new JaDiStringConverter<T>(s -> clazz.cast(new BigDecimal(s)), i -> String.valueOf(i));
+        if (Float.class.isAssignableFrom(clazz)) {
+            return new JaDiStringConverter<>(s -> clazz.cast(Float.valueOf(s)), i -> String.valueOf(i));
+        }
+        if (Double.class.isAssignableFrom(clazz)) {
+            return new JaDiStringConverter<>(s -> clazz.cast(Double.valueOf(s)), i -> String.valueOf(i));
+        }
+        if (BigDecimal.class.isAssignableFrom(clazz)) {
+            return new JaDiStringConverter<>(s -> clazz.cast(new BigDecimal(s)), i -> String.valueOf(i));
+        }
 
         throw new JaDiException("No Converter for class " + clazz + " defined");
     }
